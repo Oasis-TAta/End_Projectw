1e97d58249522d1e9d3ff9c6137d8d76bcc61a63
Shadow-web-dev380 <gjjfhjn61@gmail.com>
2026-06-06 23:31:46 +0700

    fix code

--- camera.py
+++ camera.py
@@ -4,3 +4,24 @@ import cv2
 cap1 = cv2.VideoCapture(0)
 cap2 = cv2.VideoCapture(1)
 cap3 = cv2.VideoCapture(2)
+
+while True:
+    ret1, frame1 = cap1.read()
+    ret2, frame2 = cap2.read()
+    ret3, frame3 = cap3.read()
+
+    if not ret1 or not ret2 or not ret3:
+        print("Failed to capture video")
+        break
+
+    cv2.imshow('Camera 1', frame1)
+    cv2.imshow('Camera 2', frame2)
+    cv2.imshow('Camera 3', frame3)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap1.release()
+cap2.release()
+cap3.release()
+cv2.destroyAllWindows()
