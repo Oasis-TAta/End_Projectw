c6dea2b2c3f384e3654b74621668b02e1b26acbe
Shadow-web-dev380 <gjjfhjn61@gmail.com>
2026-06-27 14:34:52 +0700

    add datetime

--- camera.py
+++ camera.py
@@ -1,5 +1,6 @@
- #main camera
+# main camera
 import cv2
+import datetime
 
 cap1 = cv2.VideoCapture(0)
 cap2 = cv2.VideoCapture(1)
@@ -10,14 +11,22 @@ while True:
     ret2, frame2 = cap2.read()
     ret3, frame3 = cap3.read()
 
-    if not ret1 or not ret2 or not ret3:
-        print("Failed to capture video")
-        break
+    # ตรวจสอบว่าสามารถอ่านเฟรมภาพได้หรือไม่
+    if ret1:
+        # พิมพ์เวลาลงบนภาพ
+        cv2.putText(frame1, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 
+                    (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+        
+        # แสดงผลภาพ
+        cv2.imshow('Camera 1', frame1)
+    else:
+        print("ไม่สามารถรับภาพจาก Camera 1 ได้")
+        break # ออกจากลูปหากไม่พบสัญญาณภาพ
 
-    cv2.imshow('Camera 1', frame1)
     cv2.imshow('Camera 2', frame2)
     cv2.imshow('Camera 3', frame3)
 
+    # กดปุ่ม 'q' เพื่อออกจากโปรแกรม (รอรับคำสั่ง 1 มิลลิวินาที)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
